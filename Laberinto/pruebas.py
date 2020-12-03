2e1af3683daa74e5f3bc8a1395ccd99c65f8e2c9
carlos-luengas-22 <60671730+carlos-luengas-22@users.noreply.github.com>
2020-12-03 13:12:22 -0600

    Update pruebas.py

--- Laberinto/pruebas.py
+++ Laberinto/pruebas.py
@@ -1,4 +1,4 @@
 from backtracking import LaberintoADt
 pasillos_inicial = ((2,1),(2,2),(2,3),(2,4),(3,2),(4,2))
-lab = LaberintoADt( 6 , 6 pasillos_inicial)
+lab = LaberintoADt( 6 , 6 pasillos_inicial, (5,2) , (2,5))
 lab.to_string()
